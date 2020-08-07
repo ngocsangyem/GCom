4ec9eb40a0f7f8c93f85dc12e5e04d0c5be1e9fd
Sang Nguyen <nnsang24@gmail.com>
2020-08-07 21:05:44 +0700

    feature: capitalizeEveryWord helper

---
+++ src/app/helpers/capitalizeEveryWord.js
@@ -0,0 +1,6 @@
+const capitalizeEveryWord = (str) =>
+	str.replace(/\b[a-z]/g, (char) => char.toUpperCase());
+
+// capitalizeEveryWord('hello world!'); // 'Hello World!'
+
+export { capitalizeEveryWord };
